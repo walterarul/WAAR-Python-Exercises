d6890ac5ed9a54f8ff405363f3548ce2dfabc041
ARUL ANAND RAJ. WALTER <70837080+walterarul@users.noreply.github.com>
2020-10-07 21:56:56 +0200

    Update Hello_World_Flask.py

--- 2020-10-06-Exercise/Hello_World_Flask.py
+++ 2020-10-06-Exercise/Hello_World_Flask.py
@@ -1,1 +1,11 @@
-as
+#Exercise 06 October 2020
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def login_page():
+    return 'Hello World'
+
+#Run/Start the server in debug mode
+if __name__ == "__main__":
+    app.run(debug=True)
